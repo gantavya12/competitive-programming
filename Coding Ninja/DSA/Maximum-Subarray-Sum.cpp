9a31967fb207578088f1620e4a1b21bd1004a7f9
gantavya12 <91743053+gantavya12@users.noreply.github.com>
2022-10-06 17:28:58 +0530

    Update Maximum-Subarray-Sum.cpp

--- Coding Ninja/DSA/Maximum-Subarray-Sum.cpp
+++ Coding Ninja/DSA/Maximum-Subarray-Sum.cpp
@@ -7,7 +7,6 @@ long long maxSubarraySum(int arr[], int n)
     long long int maxi=0;
     long long int sum=0;
     for(int i=0;i<n;i++){
-        sum=sum+arr[i];
 
         if(sum<0){
             sum=0;
